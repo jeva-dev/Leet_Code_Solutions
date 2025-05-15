c851e0dc6b1f43e06bcdfd8422c685b49b8bf6df
jeva-dev <devjeva09@gmail.com>
2025-05-15 21:12:33 +0530

    49

--- 49_Group_Anagrams.py
+++ 49_Group_Anagrams.py
@@ -7,6 +7,14 @@ class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
         anagram = defaultdict(list)
         for i in strs:
-            key = ''.join(sorted(i))
+            key = ''.join(sorted(i)) 
             anagram[key].append(i)
         return list(anagram.values())
+    
+#Word: "eat"
+#sorted("eat") → ['a', 'e', 't']
+
+#''.join(...) → "aet"
+
+#So we store:
+#anagrams["aet"] = ["eat"]
