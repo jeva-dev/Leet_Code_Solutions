96a7a0ca123d5324b520a1398dc27c5793c940b5
jeva-dev <devjeva09@gmail.com>
2025-05-23 17:28:38 +0530

    217_1

--- 217_Contains Duplicate.py
+++ 217_Contains Duplicate.py
@@ -6,8 +6,9 @@
 
 class Solution:
     def containsDuplicate(self, nums: List[int]) -> bool:
-        for i in range(len(nums)):
-            for j in range(i+1, len(nums)):
-                if nums[i] == nums[j]:
-                    return True
+        seen = set()
+        for num in nums:
+            if num in seen:
+                return True
+            seen.add(num)
         return False
