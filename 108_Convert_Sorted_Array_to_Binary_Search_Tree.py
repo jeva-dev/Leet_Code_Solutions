8c45e621ee5d46b621d4a19408d7769e9ce55b60
jeva-dev <devjeva09@gmail.com>
2025-06-12 13:13:20 +0530

    108

---
+++ 108_Convert_Sorted_Array_to_Binary_Search_Tree.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+#Input: nums = [-10,-3,0,5,9]
+#Output: [0,-3,9,-10,null,5]
+#Explanation: [0,-10,5,null,-3,null,9] is also accepted:
+
+class Solution:
+    def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
+        if not nums:
+            return None
+        
+        mid = len(nums) // 2
+        node = TreeNode(nums[mid])
+        node.left = self.sortedArrayToBST(nums[:mid])
+        node.right = self.sortedArrayToBST(nums[mid+1 :])
+        return node
